5aa6c64aa875fca5533fd7b0341b47b026c94d9f
Miguel Loza <38367845+migueloz4@users.noreply.github.com>
2019-03-28 06:35:16 -0300

    POO
    programacion orientada a objetos

---
+++ Coche.java
@@ -0,0 +1,19 @@
+package POO;
+
+public class Coche {
+	int ruedas, largo, ancho, motor, peso;  
+	
+	
+	/*ahora escribo el  metodo constructor (da el estado inicial a nuestro objeto, 
+	tiene que llamarse igual a la clase)  */
+
+	public Coche (){
+		
+		ruedas=4;
+		largo=2000;
+		ancho=300;
+		motor=1600;
+		peso=500;
+	}
+	
+} 
---
+++ Uso_Coche.java
@@ -0,0 +1,15 @@
+package POO;
+
+public class Uso_Coche {
+
+	public static void main(String[] args) {
+		
+		//ahora voy a crear un objeto (Renault) de la clase Coche
+		Coche Renault=new Coche(); // instanciar una clase, ejemplar de clase
+		
+		
+		System.out.print("Este coche tiene "+Renault.ruedas+" ruedas");
+
+	}
+
+}
